b7421318606a061b69cec4730faaabc8b4d8cdd3
Eric Bonnet <eric.bonnet@upmc.fr>
2020-06-09 02:05:15 +0200

    spring initializer

---
+++ src/test/java/fr/upmc/dri/fact/FactApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.upmc.dri.fact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
